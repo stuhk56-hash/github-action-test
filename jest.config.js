7f8777023a8c2e13f631e47c9af52938378cdc59
Mark Kim <fresh502@gmail.com>
2026-02-04 17:14:28 +0900

    테스트 성공 케이스로 수정

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  verbose: true,
+  testMatch: ["**/*.test.ts"],
   transform: {
-    ...tsJestTransformCfg,
+    "^.+\\.ts$": "ts-jest",
   },
+  moduleFileExtensions: ["ts", "js"],
 };
