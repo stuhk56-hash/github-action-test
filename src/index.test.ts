a626eec35b57b21cb8fb1cc6baf7524ccd770a03
hyeonjun <stuhk56@gmail.com>
2026-02-05 11:08:37 +0900

    테스트 코드 수정2

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import { sum } from "./index";
 
 test("1 + 2는 3이어야 합니다", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(5);
   // 테스트 실패 코드로 수정
   // expect(sum(1, 2)).toBe(5);
 });
